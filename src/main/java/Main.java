e66708232c254b14a9a557e746f923e55ec84258
Guy Miron <76874773+guymir@users.noreply.github.com>
2024-03-17 09:19:09 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,35 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+package test;
+import java.util.*;
+
+public class DoctorMain
+{
+	
+public static void doctorRating(Doctor [] doctor )
+{
+	Scanner input = new Scanner(System.in);
+	int rate = 0;
+	double sum = 0;
+	int count = 0;
+	for(int i = 0; i<doctor.length;i++)
+	{
+		while (rate!=-1)
+		{
+			 rate =input.nextInt();
+			sum+=rate;
+			count++;
+		}
+		doctor[i].setRate(sum/count);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
 }
